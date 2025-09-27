ade887b9a1f59842b69c24cd8a9c117225a19af4
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-09-27 11:52:07 -0300

    feat: real database connection on endpoints

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 from http import HTTPStatus
+from typing import cast
 
-from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, select
+from fastapi import Depends, FastAPI
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from src.database import get_session
+from src.exceptions import AppError
 from src.models import User
-from src.schemas import RootSchema, UserDb, UserPublic, UserPublicList, UserSchema
-from src.settings import Settings
+from src.schemas import RootSchema, UserPublic, UserPublicList, UserSchema
 
 app = FastAPI()
 database = []
@@ -18,41 +20,67 @@ def read_root():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    engine = create_engine(Settings().DATABASE_URL)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where((User.username == user.username) | (User.email == user.email))
+    )
 
-    with Session(engine) as session:
-        db_user = session.scalar(
-            select(User).where((User.username == user.username) | (User.email == user.email))
-        )
+    if db_user:
+        if db_user.username == user.username:
+            AppError.http_exception('BAD_REQUEST', 'Username already exists.')
+        elif db_user.email == user.email:
+            AppError.http_exception('BAD_REQUEST', 'Email already exists.')
 
-        if db_user:
-            if db_user.username == user.username:
-                ...
-            elif db_user.email == user.email:
-                ...
+    db_user = User(username=user.username, email=user.email, password=user.password)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserPublicList)
-def read_users():
-    return {'users': database}
+def read_users(limit: int = 10, offset: int = 0, session: Session = Depends(get_session)):
+    users = session.scalars(select(User).limit(limit).offset(offset))
+    return {'users': users}
+
+
+@app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def read_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = cast(User, session.scalar(select(User).where(User.id == user_id)))
+
+    if not db_user:
+        AppError.http_exception('NOT_FOUND', 'This user does not exists')
+
+    return db_user
 
 
 @app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    user_with_id = UserDb(**user.model_dump(), id=user_id)
+def update_user(user_id: int, user: UserSchema, session: Session = Depends(get_session)):
+    db_user = cast(User, session.scalar(select(User).where(User.id == user_id)))
 
-    if user_id < 1 or user_id > len(database):
-        raise HTTPException(detail='Not found!', status_code=HTTPStatus.NOT_FOUND)
+    if not db_user:
+        AppError.http_exception('NOT_FOUND', 'This user does not exists')
 
-    database[user_id - 1] = user_with_id
+    db_user.username = user.username
+    db_user.email = user.email
+    db_user.password = user.password
 
-    return user_with_id
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
-@app.delete('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
-def delete_user(user_id: int):
-    if user_id < 1 or user_id > len(database):
-        raise HTTPException(detail='Not found!', status_code=HTTPStatus.NOT_FOUND)
+@app.delete('/users/{user_id}', status_code=HTTPStatus.OK)
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
 
-    return database.pop(user_id - 1)
+    if not db_user:
+        AppError.http_exception('NOT_FOUND', 'This user does not exists')
+
+    session.delete(db_user)
+    session.commit()
+
+    return {'message': 'User deleted'}

ad458e69188d5b9c5846b0c6bb38afe16ea2e67c
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-09-27 11:59:27 -0300

    tests: sesion and client fixtures, endpoint tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,25 +5,42 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from src.app import app
+from src.database import get_session
 from src.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:', connect_args={'check_same_thread': False}, poolclass=StaticPool
+    )
+
+    connection = engine.connect()
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
         yield session
 
+    session.close()
     table_registry.metadata.drop_all(engine)
+    connection.close()
+    engine.dispose()
 
 
 @contextmanager
@@ -44,3 +61,13 @@ def _mock_db_time(model, time=datetime(2025, 5, 20)):
 @pytest.fixture
 def mock_db_time():
     return _mock_db_time
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='jonh', email='jonh@gmail.com', password='secret')
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from src.schemas import UserPublic
+
 
 def test_root_returns_hello_world(client):
     response = client.get('/')
@@ -16,22 +18,61 @@ def test_create_user(client):
     assert response.json() == {'id': 1, 'email': 'jonh@gmail.com', 'username': 'jonh'}
 
 
+def test_create_user_username_already_exists(client, user):
+    payload = {'username': 'jonh', 'email': 'jonh@gmail.com', 'password': '123'}
+    response = client.post('/users/', json=payload)
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username already exists.'}
+
+
+def test_create_user_email_already_exists(client, user):
+    payload = {'username': 'jonathas', 'email': 'jonh@gmail.com', 'password': '123'}
+    response = client.post('/users/', json=payload)
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already exists.'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'users': [{'id': 1, 'email': 'jonh@gmail.com', 'username': 'jonh'}]}
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
-    payload = {'username': 'alice', 'email': 'alice@gmail.com', 'password': 'secret'}
+def test_read_user_by_id(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == user_schema
+
+
+def test_read_user_by_id_not_found(client):
+    response = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'This user does not exists'}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
+    payload = {'username': 'jonh', 'email': 'jonh@gmail.com', 'password': 'secret'}
     response = client.put('/users/1', json=payload)
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         'id': 1,
-        'username': 'alice',
-        'email': 'alice@gmail.com',
+        'username': 'jonh',
+        'email': 'jonh@gmail.com',
     }
 
 
@@ -40,21 +81,18 @@ def test_update_not_found_user(client):
     response = client.put('/users/2', json=payload)
 
     assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'This user does not exists'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'id': 1,
-        'username': 'alice',
-        'email': 'alice@gmail.com',
-    }
+    assert response.json() == {'message': 'User deleted'}
 
 
 def test_delete_not_found_user(client):
-    payload = {'username': 'alice', 'email': 'alice@gmail.com', 'password': 'secret'}
-    response = client.put('/users/2', json=payload)
+    response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'This user does not exists'}

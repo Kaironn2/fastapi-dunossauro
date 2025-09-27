32209ac387d507c2e02e19ed7bb95a739f206042
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-09-27 11:58:52 -0300

    fix: settings database_url type error fixed

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,7 @@
-from typing import ClassVar
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
-    DATABASE_URL: ClassVar[str]
+    DATABASE_URL: str
